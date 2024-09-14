547e91eacd1c6680632162955447e5737e4578b2
peepeds <edbertmac@gmail.com>
2024-09-14 23:42:41 +0700

    fix : month naming

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ setInterval(() => {
     let sec = date.getSeconds();
 
     let currentDate = date.getDate();
-    let ArrMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let ArrMonth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     let currentMonth = date.getMonth();
     let currentYear = date.getFullYear();
 
