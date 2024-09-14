a8d3351effb3d6c74d46b49e867f779e1ff62266
peepeds <edbertmac@gmail.com>
2024-09-14 22:49:21 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const hour = document.getElementById("hour");
+const minute = document.getElementById("min");
+const second = document.getElementById("sec");
+const day = document.getElementById("day");
+const date_time = document.getElementById("date");
+
+setInterval(() => {
+    let date = new Date();
+
+    let ArrDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let currentDay = date.getDay();
+
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    let currentDate = date.getDate();
+    let ArrMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let currentMonth = date.getMonth();
+    let currentYear = date.getFullYear();
+
+    hr = (hr < 10) ? `0${hr}` : hr;
+    min = (min < 10) ? `0${min}` : min;
+    sec = (sec < 10) ? `0${sec}` : sec;
+
+    day.innerHTML = ArrDay[currentDay];
+
+    hour.textContent = hr;
+    minute.textContent = min;
+    second.textContent = sec;
+
+    date_time.innerHTML = `${currentDate} ${ArrMonth[currentMonth]} ${currentYear}`;
+
+}, 1000);
